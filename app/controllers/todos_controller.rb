788fa1933775f7067fc35e21d202da8746a9be2f
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-24 16:42:47 +0100

    ✨feat(todo_controller): creation de set_todo et todo_params

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,11 @@
 class TodosController < ApplicationController
+  before_action :set_todo only[:show, :update, :destroy]
+  private
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:id, :title, :completed)
+  end
 end
