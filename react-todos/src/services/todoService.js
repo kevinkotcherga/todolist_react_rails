487b7bfca5097b062cc3cfc00fade1e79f1caff6
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-25 11:04:08 +0100

    ✨feat(todoService): configuration json react

--- react-todos/src/services/todoService.js
+++ react-todos/src/services/todoService.js
@@ -34,3 +34,9 @@ export const updateTodo = (todo) => {
     }),
   }).then((res) => res.json());
 };
+
+export const deleteTodo = (id) => {
+  return fetch(`${baseUrl}/${id}`, {
+    method: "DELETE",
+  }).then(res => res.json());
+}
